f89ac4e3fe23628385552bfb8e8c86089a01891a
angkhanalil <34016307+angkhanalil@users.noreply.github.com>
2022-02-24 00:18:22 +0700

    feat: Create Customer

---
+++ src/main/java/com/example/shoppingapi/model/Customer.java
@@ -0,0 +1,79 @@
+package com.example.shoppingapi.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.websocket.OnError;
+import java.util.List;
+
+@Entity
+public class Customer {
+    @Id
+    private Integer id;
+    private String username; //email
+    private String password;
+
+    private String firstname;
+    private String lastname;
+    private String phoneno;
+
+    @OneToMany
+    private List<CustomerAddressItem> customerAddress;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+
+    public void setPhoneno(String phoneno) {
+        this.phoneno = phoneno;
+    }
+
+    public String getPhoneno() {
+        return phoneno;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setCustomerAddress(List<CustomerAddressItem> customerAddress) {
+        this.customerAddress = customerAddress;
+    }
+
+    public List<CustomerAddressItem> getCustomerAddress() {
+        return customerAddress;
+    }
+}
