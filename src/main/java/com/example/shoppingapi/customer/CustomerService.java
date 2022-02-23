ae7f599fa6481812f9d370464bf3df9f609bfac3
angkhanalil <34016307+angkhanalil@users.noreply.github.com>
2022-02-24 00:55:45 +0700

    feat: edit check username and password add CustomerAll()

--- src/main/java/com/example/shoppingapi/customer/CustomerService.java
+++ src/main/java/com/example/shoppingapi/customer/CustomerService.java
@@ -1,23 +1,39 @@
 package com.example.shoppingapi.customer;
 
 import com.example.shoppingapi.model.Customer;
+import com.example.shoppingapi.product.ResponseProduct;
 import org.jetbrains.annotations.NotNull;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class CustomerService {
 
+    @Autowired
     private CustomerRepository customerRepository;
 
     public ResponseEntity<Customer> ShowCustomerData(@NotNull RequestSignIn signIn) {
         String username = signIn.getEmail();
-        Customer result = customerRepository.findByUsername(username);
+        String password = signIn.getPassword();
+        Customer result = customerRepository.findByUsernameAndPassword(username,password);
         if (result != null) {
             return new ResponseEntity<>(result, HttpStatus.OK) ;
         }
         throw new CustomerNotFoundException(username);
+
+    }
+
+    public ResponseEntity<List<Customer>> CustomerAll() {
+
+        List<Customer> result = customerRepository.findAll();
+        if(!result.isEmpty()){
+            return new ResponseEntity<>(result, HttpStatus.OK) ;
+        }
+        throw new CustomerNotFoundException("Customer");
 
     }
 
