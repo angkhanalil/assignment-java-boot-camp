32b53d33b3c8be2a91f657ff185a881ecc779190
angkhanalil <34016307+angkhanalil@users.noreply.github.com>
2022-02-24 00:53:04 +0700

    feat: function
    1. check username and password
    2. add findAll()

--- src/main/java/com/example/shoppingapi/customer/CustomerRepository.java
+++ src/main/java/com/example/shoppingapi/customer/CustomerRepository.java
@@ -4,8 +4,14 @@ import com.example.shoppingapi.model.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
-public interface CustomerRepository extends JpaRepository<Customer,Integer> {
+import java.util.List;
+import java.util.Optional;
 
-    @Query("select c from Customer c where c.username = ?1")
-    Customer findByUsername(String username);
+public interface CustomerRepository extends JpaRepository<Customer, Integer> {
+
+    //    @Query("select c from Customer c where c.username = ?1")
+//    @Query("select c from Customer c where c.username = ?1 and c.password = ?2")
+    Customer findByUsernameAndPassword(String username, String password);
+
+    List<Customer> findAll();
 }
