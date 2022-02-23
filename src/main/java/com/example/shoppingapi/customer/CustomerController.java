f20483d44b6324e5a3d23a602e4b771f06e04222
angkhanalil <34016307+angkhanalil@users.noreply.github.com>
2022-02-24 00:56:32 +0700

    feat: new Endpoint /customer

--- src/main/java/com/example/shoppingapi/customer/CustomerController.java
+++ src/main/java/com/example/shoppingapi/customer/CustomerController.java
@@ -3,9 +3,9 @@ package com.example.shoppingapi.customer;
 import com.example.shoppingapi.model.Customer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class CustomerController {
@@ -13,9 +13,15 @@ public class CustomerController {
     @Autowired
     private CustomerService customerService;
 
-    @PostMapping("/SignIn")
+    @PostMapping("/customer/SignIn")
     public ResponseEntity<Customer> SignIn(@RequestBody RequestSignIn singin) {
         // Sing in with email
         return customerService.ShowCustomerData(singin);
     }
+
+    @GetMapping("/customer")
+    public ResponseEntity<List<Customer>> findCustomerAll() {
+        return customerService.CustomerAll();
+    }
+
 }
