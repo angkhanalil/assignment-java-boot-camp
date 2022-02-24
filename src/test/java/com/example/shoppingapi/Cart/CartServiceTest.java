d8c366f7022e82c0a75e6e311b837ad312c34295
angkhanalil <34016307+angkhanalil@users.noreply.github.com>
2022-02-24 22:33:36 +0700

    feat: Create CartServiceTest

---
+++ src/test/java/com/example/shoppingapi/Cart/CartServiceTest.java
@@ -0,0 +1,16 @@
+package com.example.shoppingapi.Cart;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CartServiceTest {
+
+    @Test
+    void findCartByUserid() {
+    }
+
+    @Test
+    void addtoCart() {
+    }
+}
