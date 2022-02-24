ef521d9d10ed471c96e3823223fa24d44025b88c
angkhanalil <34016307+angkhanalil@users.noreply.github.com>
2022-02-24 22:33:23 +0700

    feat: Create CartRepositoryTest

---
+++ src/test/java/com/example/shoppingapi/Cart/CartRepositoryTest.java
@@ -0,0 +1,12 @@
+package com.example.shoppingapi.Cart;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CartRepositoryTest {
+
+    @Test
+    void findByUserid() {
+    }
+}
