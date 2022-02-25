1ce76bab561ab88a3a3adce6a9a11b5d07d4b470
angkhanalil <34016307+angkhanalil@users.noreply.github.com>
2022-02-26 00:19:39 +0700

    feat: Create CustomerAddressItemRepository

---
+++ src/main/java/com/example/shoppingapi/CustomerAddressItem/CustomerAddressItemRepository.java
@@ -0,0 +1,7 @@
+package com.example.shoppingapi.CustomerAddressItem;
+
+import com.example.shoppingapi.model.CustomerAddressItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CustomerAddressItemRepository extends JpaRepository<CustomerAddressItem,Integer> {
+}
