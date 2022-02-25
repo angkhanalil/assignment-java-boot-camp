ab785c1e480a5367995d32a544277e1f77ec3f3a
angkhanalil <34016307+angkhanalil@users.noreply.github.com>
2022-02-26 00:16:28 +0700

    del: del User class --> change to use customer

--- src/main/java/com/example/shoppingapi/user/UserNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.example.shoppingapi.user;
-
-public class UserNotFoundException extends RuntimeException {
-
-    public UserNotFoundException(String name){
-        super(name);
-    }
-}
