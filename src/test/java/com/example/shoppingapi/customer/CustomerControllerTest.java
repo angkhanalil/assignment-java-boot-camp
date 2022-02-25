c19b8da144fa7737e4ac7117ca8acbcaae2147af
angkhanalil <34016307+angkhanalil@users.noreply.github.com>
2022-02-26 01:01:09 +0700

    test: new create test

--- src/test/java/com/example/shoppingapi/customer/CustomerControllerTest.java
+++ src/test/java/com/example/shoppingapi/customer/CustomerControllerTest.java
@@ -1,14 +1,16 @@
 package com.example.shoppingapi.customer;
 
 import com.example.shoppingapi.model.Customer;
+import com.example.shoppingapi.model.CustomerAddressItem;
+import com.example.shoppingapi.product.ProductController;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -17,38 +19,22 @@ import static org.mockito.Mockito.when;
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class CustomerControllerTest {
 
+    @InjectMocks
+    private CustomerController  customerController;
+
     @Autowired
     private TestRestTemplate testRestTemplate;
 
-    @MockBean
+    @Autowired
     private CustomerRepository customerRepository;
+
 
     @Test
     void signIn() {
-        //Arrange
-        Customer customer = new Customer();
-        customer.setId(1);
-        customer.setUsername("abc@gmail.com");
-        customer.setPassword("1234");
-        customer.setFirstname("Angkhana");
-        customer.setLastname("Lila");
-        customer.setPhoneno("0999999999");
-
-        RequestSignIn requestSignIn = new RequestSignIn();
-        requestSignIn.setEmail("abc@gmail.com");
-        requestSignIn.setPassword("1234");
-
-        when(customerRepository.findByUsernameAndPassword("abc@gmail.com","1234")).thenReturn(customer);
-
-        //Act
-        Customer result = testRestTemplate.postForObject("/customer/SignIn", requestSignIn, Customer.class);
-        //Assert
-        assertNotNull(result);
 
     }
 
     @Test
     void findCustomerAll() {
-
     }
 }
--- src/test/java/com/example/shoppingapi/customer/CustomerRepositoryTest.java
+++ src/test/java/com/example/shoppingapi/customer/CustomerRepositoryTest.java
@@ -1,10 +1,13 @@
 package com.example.shoppingapi.customer;
 
 import com.example.shoppingapi.model.Customer;
+import com.example.shoppingapi.model.CustomerAddressItem;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -13,6 +16,9 @@ class CustomerRepositoryTest {
 
     @Autowired
     private CustomerRepository customerRepository;
+
+    @Autowired
+    private TestEntityManager entityManager;
 
     @Test
     void findByUsernameAndPassword() {
