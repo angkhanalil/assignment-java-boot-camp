21a72ad1c3ab4269d30d1cbd97e40be681c0a788
angkhanalil <34016307+angkhanalil@users.noreply.github.com>
2022-02-26 03:26:04 +0700

    test: test cast show detail in basket

--- src/test/java/com/example/shoppingapi/Cart/CartControllerTest.java
+++ src/test/java/com/example/shoppingapi/Cart/CartControllerTest.java
@@ -1,16 +1,50 @@
 package com.example.shoppingapi.Cart;
 
+import com.example.shoppingapi.model.Cart;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.web.client.TestRestTemplate;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.ArrayList;
+import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.when;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class CartControllerTest {
+    @Autowired
+    private TestRestTemplate testRestTemplate;
+
+    @MockBean
+    private CartRepository cartRepository;
 
     @Test
     void addtocart() {
+
+
     }
 
     @Test
+    @DisplayName("show basket")
     void cart() {
+//Arrange
+        List<Cart> cartList = new ArrayList<>();
+        Cart cart = new Cart();
+        cart.setCartid(1);
+        cart.setItemid(1);
+        cart.setItemqty(1);
+        cart.setUserid(1);
+        cartList.add(cart);
+        when(cartRepository.findByUserid(1)).thenReturn(cartList);
+
+        //Act
+        String result = testRestTemplate.getForObject("/cart/1", String.class);
+        //assert
+        System.out.println(result.isEmpty());
+        assertNotNull(result.isEmpty());
     }
 }
