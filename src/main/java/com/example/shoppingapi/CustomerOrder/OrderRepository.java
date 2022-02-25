ffd1b6776126b20fa2cedd97c1aa69bdfe71066f
angkhanalil <34016307+angkhanalil@users.noreply.github.com>
2022-02-26 00:51:45 +0700

    feat: Create OrderRepository

--- src/main/java/com/example/shoppingapi/CustomerOrder/OrderRepository.java
+++ src/main/java/com/example/shoppingapi/CustomerOrder/OrderRepository.java
@@ -1,4 +1,4 @@
-package com.example.shoppingapi.Order;
+package com.example.shoppingapi.CustomerOrder;
 
 import com.example.shoppingapi.model.CustomerOrder;
 import org.springframework.data.jpa.repository.JpaRepository;
