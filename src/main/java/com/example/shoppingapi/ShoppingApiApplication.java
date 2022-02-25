dfa29767471dc92760ff014134eea9698977d619
angkhanalil <34016307+angkhanalil@users.noreply.github.com>
2022-02-26 03:36:32 +0700

    fix: PostConstruct

--- src/main/java/com/example/shoppingapi/ShoppingApiApplication.java
+++ src/main/java/com/example/shoppingapi/ShoppingApiApplication.java
@@ -9,6 +9,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import javax.annotation.PostConstruct;
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootApplication
 public class ShoppingApiApplication {
@@ -20,6 +22,18 @@ public class ShoppingApiApplication {
 
     @PostConstruct
     public void xyz() {
+        CustomerAddressItem addressItem = new CustomerAddressItem();
+        addressItem.setAddess("address1");
+        addressItem.setFullname("name1");
+
+        CustomerAddressItem addressItem2 = new CustomerAddressItem();
+        addressItem2.setAddess("address2");
+        addressItem2.setFullname("name2");
+
+        List<CustomerAddressItem> customerAddressItemList = new ArrayList<>();
+        customerAddressItemList.add(addressItem);
+        customerAddressItemList.add(addressItem2);
+
         Customer customer = new Customer();
         customer.setId(1);
         customer.setUsername("abc@gmail.com");
@@ -29,24 +43,10 @@ public class ShoppingApiApplication {
         customer.setPhoneno("0999999999");
         customerRepository.save(customer);
 
-        CustomerAddressItem addressItem = new CustomerAddressItem();
-        Customer cust  = customerRepository.findByUsernameAndPassword("abc@gmail.com","1234");
-        addressItem.setCustomerid(cust.getId());
-        addressItem.setAddess("address1");
-        addressItem.setFullname("name1");
 
-        CustomerAddressItem addressItem2 = new CustomerAddressItem();
-        addressItem2.setCustomerid(cust.getId());
-        addressItem2.setAddess("address2");
-        addressItem2.setFullname("name2");
-
-
-        customerAddressItemRepository.save(addressItem);
-        customerAddressItemRepository.save(addressItem2);
-
-
-        System.out.println("save");
-
+        customer.setCustomerAddress(customerAddressItemList);
+        Customer cust = customerRepository.save(customer);
+        System.out.print(cust);
     }
 
     public static void main(String[] args) {
