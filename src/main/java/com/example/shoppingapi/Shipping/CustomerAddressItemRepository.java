fce994830436286b4cfb5055bd09817ec319e33a
angkhanalil <34016307+angkhanalil@users.noreply.github.com>
2022-02-27 00:53:56 +0700

    feat: findByCustomerid

--- src/main/java/com/example/shoppingapi/Shipping/CustomerAddressItemRepository.java
+++ src/main/java/com/example/shoppingapi/Shipping/CustomerAddressItemRepository.java
@@ -1,7 +1,12 @@
-package com.example.shoppingapi.CustomerAddressItem;
+package com.example.shoppingapi.Shipping;
 
 import com.example.shoppingapi.model.CustomerAddressItem;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CustomerAddressItemRepository extends JpaRepository<CustomerAddressItem,Integer> {
+
+    List<CustomerAddressItem>  findByCustomerid(Integer customerid);
+
 }
