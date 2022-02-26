4d49331f313c37acee161123fa6dc1b987db9f84
angkhanalil <34016307+angkhanalil@users.noreply.github.com>
2022-02-27 00:55:59 +0700

    fix: return type findByOrderid

--- src/main/java/com/example/shoppingapi/CustomerOrder/CustomerOrderItemRepository.java
+++ src/main/java/com/example/shoppingapi/CustomerOrder/CustomerOrderItemRepository.java
@@ -2,6 +2,12 @@ package com.example.shoppingapi.CustomerOrder;
 
 import com.example.shoppingapi.model.CustomerOrderItem;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface CustomerOrderItemRepository extends JpaRepository<CustomerOrderItem ,Integer> {
+
+//    @Query("select c from CustomerOrderItem c where c.orderid = ?1")
+    List<CustomerOrderItem> findByOrderid(String orderid);
 }
