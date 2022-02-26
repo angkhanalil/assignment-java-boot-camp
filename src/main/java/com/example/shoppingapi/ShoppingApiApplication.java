cd32192deb70947e9c883fc7c82476e279f227f0
angkhanalil <34016307+angkhanalil@users.noreply.github.com>
2022-02-27 01:01:16 +0700

    fix: edit PostConstruct func.

--- src/main/java/com/example/shoppingapi/ShoppingApiApplication.java
+++ src/main/java/com/example/shoppingapi/ShoppingApiApplication.java
@@ -1,16 +1,13 @@
 package com.example.shoppingapi;
 
-import com.example.shoppingapi.CustomerAddressItem.CustomerAddressItemRepository;
+import com.example.shoppingapi.Shipping.CustomerAddressItemRepository;
 import com.example.shoppingapi.customer.CustomerRepository;
 import com.example.shoppingapi.model.Customer;
-import com.example.shoppingapi.model.CustomerAddressItem;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import javax.annotation.PostConstruct;
-import java.util.ArrayList;
-import java.util.List;
 
 @SpringBootApplication
 public class ShoppingApiApplication {
@@ -22,33 +19,18 @@ public class ShoppingApiApplication {
 
     @PostConstruct
     public void xyz() {
-        CustomerAddressItem addressItem = new CustomerAddressItem();
-        addressItem.setAddess("address1");
-        addressItem.setFullname("name1");
-
-        CustomerAddressItem addressItem2 = new CustomerAddressItem();
-        addressItem2.setAddess("address2");
-        addressItem2.setFullname("name2");
-
-        List<CustomerAddressItem> customerAddressItemList = new ArrayList<>();
-        customerAddressItemList.add(addressItem);
-        customerAddressItemList.add(addressItem2);
 
         Customer customer = new Customer();
-        customer.setId(1);
+//        customer.setId();
         customer.setUsername("abc@gmail.com");
         customer.setPassword("1234");
         customer.setFirstname("Angkhana");
         customer.setLastname("Lila");
         customer.setPhoneno("0999999999");
-        customerRepository.save(customer);
 
-
-        customer.setCustomerAddress(customerAddressItemList);
         Customer cust = customerRepository.save(customer);
-        System.out.print(cust);
+        System.out.print("Save");
     }
-
     public static void main(String[] args) {
         SpringApplication.run(ShoppingApiApplication.class, args);
         System.out.println("TEST");
