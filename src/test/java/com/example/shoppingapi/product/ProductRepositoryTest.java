e7f25592ee6b5da854c6bf969411195c9aa8087d
angkhanalil <34016307+angkhanalil@users.noreply.github.com>
2022-02-27 01:57:56 +0700

    test : edit testcase result

--- src/test/java/com/example/shoppingapi/product/ProductRepositoryTest.java
+++ src/test/java/com/example/shoppingapi/product/ProductRepositoryTest.java
@@ -48,7 +48,7 @@ class ProductRepositoryTest {
     @Test
     void findByProductid_With_NotFound() {
         //Act
-        Product result = productRepository.findByProductid(2);
+        Product result = productRepository.findByProductid(6);
         //Assert
         assertNull(result);
 
@@ -56,18 +56,45 @@ class ProductRepositoryTest {
 
     @Test
     void findByProductNameContaining_With_success() {
+        Product product1 = new Product();
+        product1.setProductid(1);
+        product1.setProductName("adidas ORIGINALS Stan Smith Shoes Women White");
+        product1.setPrice(1792.00);
+        product1.setBrand("adidas");
+        product1.setProductDetails("Despite their history on the tennis courts, you don't need a racket and short shorts to appreciate the clean-cut style of the adidas Stan Smith trainer.");
+        product1.setCategory("shoes");
+        product1.setSku("");
+        productRepository.save(product1);
+
+        Product product2 = new Product();
+        product2.setProductid(2);
+        product2.setProductName("adidas ORIGINALS NMD_R1");
+        product2.setPrice(2576.00);
+        product2.setBrand("adidas");
+        product2.setProductDetails("Continue the NMD legacy in these adidas NMD_R1 Shoes");
+        product2.setCategory("shoes");
+        product2.setSku("2282329674_TH-7670241944");
+        productRepository.save(product2);
         //Act
         List<Product> result = productRepository.findByProductNameContainingIgnoreCase("Adidas");
         //Assert
         assertNotNull(result);
-        assertEquals(1,result.size());
+        assertEquals(2,result.size());
 
     }
 
     @Test
     void findByProductNameContaining_With_failed() {
+        Product product  = new Product();
+        product.setProductid(1);
+        product.setProductName("Adidas NMD1");
+        product.setPrice(3000.00);
+        product.setBrand("ADIDAS");
+        product.setProductDetails("detail");
+        productRepository.save(product);
+
         //Act
-        List<Product> result = productRepository.findByProductNameContainingIgnoreCase("Nike");
+        List<Product> result = productRepository.findByProductNameContainingIgnoreCase("Nivea");
         System.out.println("result.size() " + result.size());
         //Assert
         assertEquals(0,result.size());
