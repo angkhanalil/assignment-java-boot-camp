e0cfa123322e14a4c32111bbd39fee5b3423b2a2
angkhanalil <34016307+angkhanalil@users.noreply.github.com>
2022-02-27 01:00:46 +0700

    fix: ShowCustomerData

--- src/main/java/com/example/shoppingapi/customer/CustomerService.java
+++ src/main/java/com/example/shoppingapi/customer/CustomerService.java
@@ -1,14 +1,17 @@
 package com.example.shoppingapi.customer;
 
+import com.example.shoppingapi.Shipping.CustomerAddressItemService;
 import com.example.shoppingapi.model.Customer;
-import com.example.shoppingapi.product.ResponseProduct;
+import com.example.shoppingapi.model.CustomerAddressItem;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.PostConstruct;
 import java.util.List;
+import java.util.Set;
 
 @Service
 public class CustomerService {
@@ -16,18 +19,22 @@ public class CustomerService {
     @Autowired
     private CustomerRepository customerRepository;
 
+    @Autowired
+    private CustomerAddressItemService customerAddressItemService;
+
     public void setCustomerRepository(CustomerRepository customerRepository) {
         this.customerRepository = customerRepository;
     }
 
+
+
     public ResponseEntity<Customer> ShowCustomerData(@NotNull RequestSignIn signIn) {
         String username = signIn.getEmail();
         String password = signIn.getPassword();
-        Customer result = customerRepository.findByUsernameAndPassword(username,password);
+        Customer result = customerRepository.findByUsernameAndPassword(username, password);
 
         if (result != null) {
-
-            return new ResponseEntity<>(result, HttpStatus.OK) ;
+            return new ResponseEntity<>(result, HttpStatus.OK);
         }
         throw new CustomerNotFoundException(username);
 
@@ -36,8 +43,8 @@ public class CustomerService {
     public ResponseEntity<List<Customer>> CustomerAll() {
 
         List<Customer> result = customerRepository.findAll();
-        if(!result.isEmpty()){
-            return new ResponseEntity<>(result, HttpStatus.OK) ;
+        if (!result.isEmpty()) {
+            return new ResponseEntity<>(result, HttpStatus.OK);
         }
         throw new CustomerNotFoundException("Customer");
 
