9d40975ec07dc03e177d20bfc47512b336fbda99
angkhanalil <34016307+angkhanalil@users.noreply.github.com>
2022-02-27 01:02:18 +0700

    feat:  add new method -findByOrderid -findByUserid

--- src/main/java/com/example/shoppingapi/CustomerOrder/OrderRepository.java
+++ src/main/java/com/example/shoppingapi/CustomerOrder/OrderRepository.java
@@ -2,7 +2,15 @@ package com.example.shoppingapi.CustomerOrder;
 
 import com.example.shoppingapi.model.CustomerOrder;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface OrderRepository extends JpaRepository<CustomerOrder,Integer> {
 
+//    @Query("select c from CustomerOrder c where c.orderid = ?1")
+    CustomerOrder findByOrderid(String orderid);
+
+//    @Query("select c from CustomerOrder c where c.userid = ?1")
+    List<CustomerOrder> findByUserid(Integer userid);
 }
